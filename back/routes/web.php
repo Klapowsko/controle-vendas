bb3b4bda43ef59fc0b22b32a8255063648d93ff0
Hiago <hiagoklapowsko@gmail.com>
2020-08-30 17:37:14 -0300

    iniciado endpoints

--- back/routes/web.php
+++ back/routes/web.php
@@ -13,6 +13,8 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->get('clientes', function () {
+        echo 'users';
+    });
 });
